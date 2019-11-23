3dd2a6afd5ed2c0dd2e8dfa3cc61c7d1bb06d496
Sachin Kumar Singh <sachinkumarsingh092@gmail.com>
2019-11-23 23:29:19 +0530

    Added features

---
+++ setenv.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+export DATABASE_URL=postgres://rorzmqnunwbxqz:2c4a49ddb3468a935ec9a0d3475e40c51382b09d27a22623253cafd7593737ef@ec2-107-21-97-5.compute-1.amazonaws.com:5432/d23uqnpja601mh;
+echo "Database set";
+export FLASK_APP=application.py;
+echo "App set";
+export FLASK_DEBUG=1;
+echo "Debug set"
